fac0c206d109d9cebe25611db817a1111b0a0a07
Kinga Pszeniczna <w57122@student.wsiz.rzeszow.pl>
2019-10-10 21:18:31 +0200

    Dodaj pliki projektów.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="seminarium.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(seminarium.Startup))]
+namespace seminarium
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
